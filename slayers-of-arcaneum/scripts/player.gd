0aa50331af80065f9bb11add2d01bd647e0df575
Carl <Carl.heintz02@gmail.com>
2024-10-25 11:06:26 +0200

    ajout dossier

--- slayers-of-arcaneum/scripts/player.gd
+++ slayers-of-arcaneum/scripts/player.gd
@@ -1,13 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 150
-signal health_depleted
-var hp = 100.0
-
-
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-var is_attacking = false
-
+@export var speed = 130.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -21,35 +14,6 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-	
-	const DAMAGE_RATE = 5.0
-	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
-	if overlapping_mobs.size() > 0:
-		hp -= DAMAGE_RATE * overlapping_mobs.size() * delta
-		%ProgressBar.value = hp
-		if hp <= 0.0:
-			health_depleted.emit()
-
-	# Flip the sprite based on movement direction
-	if velocity.x > 0:
-		animated_sprite.flip_h = false
-	elif velocity.x < 0:
-		animated_sprite.flip_h = true
-
-	# Si on n'est pas en train d'attaquer, jouer les animations de déplacement
-	if not is_attacking:
-		if velocity.length() == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("running")
-	
-	# Normaliser la vitesse si le personnage se déplace
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-
-	position += velocity * delta
-	
+	velocity = velocity.normalized() * speed
+	move_and_slide()
 	# Gestion du délai avant de quitter le jeu
-	if Input.is_action_pressed("quit_game"):
-		print("appuie sur Quit (Touche B8) effectue")
-		JavaScriptBridge.eval("window.location.href='http://localhost:3000'")
